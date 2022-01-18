117a06cb16c2e1a47b0ea8c69e30925da8c09b74
Ankur Wasnik <ankurwasnik358@gmail.com>
2022-01-18 22:17:25 +0530

    dynamic year fixed

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+/*
 var btn_hireme = document.getElementById('btn-hire');
 btn_hireme.onclick = function(){
      alert("You can email me at ankurwasnik358@gmail.com");
 }
+*/
+
+var element = document.getElementById('currentYear') ;
+var currentYear = new Date().getFullYear();
+var string = "Copyright Ankur Wasnik ";
+string = string.concat(currentYear);
+element.innerHTML = string ;
